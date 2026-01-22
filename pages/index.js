a78491cb44e7c68a4b669be1b887d0a99b9d78a1
JeanRGarcia <97805462+JeanRGarcia@users.noreply.github.com>
2026-01-22 16:33:29 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-function Home(){
-    return (
-        <>
-            <h1>Se tu ta lendo isso, saiba que ta dando certo</h1>
-            <p>Comecei agora esse curso.dev. Aparentemente ta tudo dando certo, porem, preciso subir esse site cocozinho em uma hospedagem.</p>
-            <p>Agora me responde: deu certo mesmo ou estou pirando?</p>
-            <p>Pelo visto deu certo, a reação foi boa!</p>
-        </>
-    )
+function Home() {
+  return (
+    <>
+      <h1>Se tu ta lendo isso, saiba que ta dando certo</h1>
+      <p>
+        Comecei agora esse curso.dev. Aparentemente ta tudo dando certo, porem,
+        preciso subir esse site cocozinho em uma hospedagem.
+      </p>
+      <p>Agora me responde: deu certo mesmo ou estou pirando?</p>
+      <p>Pelo visto deu certo, a reação foi boa!</p>
+    </>
+  );
 }
 
 export default Home;
